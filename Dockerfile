92002b6c073e110be458253b4ade1652585766f8
Miguel Angel Luna <miluna1994@gmail.com>
2019-06-05 19:56:05 +0200

    environment variables

--- Dockerfile
+++ Dockerfile
@@ -11,7 +11,33 @@ RUN npm run build
 FROM node:10
 COPY package*.json .
 COPY --from=builder build .
+
 ARG port=8080
-ENV $PORT port
-EXPOSE $PORT
+ENV PORT=$port
+
+ARG db_host='localhost'
+ENV DB_HOST=$db_host
+
+ARG db_port=3360
+ENV DB_PORT=$db_port
+
+ARG db_username='root'
+ENV DB_USERNAME=$db_username
+
+ARG db_password='root'
+ENV DB_PASSWORD=$db_password
+
+ARG db_name='twitter'
+ENV DB_NAME=$db_name
+
+ARG service_email=''
+ENV SERVICE_EMAIL=$service_email
+
+ARG service_email_password=''
+ENV SERVICE_EMAIL_PASSWORD=$service_email_password
+
+ARG jwt_secret='secret'
+ENV JWT_SECRET=$jwt_secret
+
+EXPOSE $port
 CMD [ "npm", "start" ]
--- src/config/config.ts
+++ src/config/config.ts
@@ -3,18 +3,18 @@ import { Transporter, createTransport } from "nodemailer";
 import mysql from 'promise-mysql';
 
 const dbConfig: ConnectionConfig = {
-    host: process.env.DB_HOST || 'localhost',
-    port: parseInt(process.env.DB_PORT) || 3360,
-    user: process.env.DB_USERNAME || 'root',
-    password: process.env.DB_PASSWORD || 'root',
-    database: process.env.DB_NAME || 'twitter'
+    host: process.env.DB_HOST,
+    port: parseInt(process.env.DB_PORT),
+    user: process.env.DB_USERNAME,
+    password: process.env.DB_PASSWORD,
+    database: process.env.DB_NAME,
 };
 export const db = mysql.createPool(dbConfig);
-export const secretOrKey: string = "secret";
+export const secretOrKey: string = process.env.JWT_SECRET;
 export const emailConfig: Transporter = createTransport({
     service: 'gmail',
     auth: {
-        user: process.env.SERVICE_EMAIL || 'youremail@gmail.com',
-        pass: process.env.SERVICE_EMAIL_PASSWORD || 'yourpassword',
+        user: process.env.SERVICE_EMAIL,
+        pass: process.env.SERVICE_EMAIL_PASSWORD,
     }
 });
